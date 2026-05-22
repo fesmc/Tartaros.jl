61c52b8c686187a6418c25a724c4859b535a096f
JanJereczek <jan.jereczek@gmail.com>
2026-05-22 15:29:45 +0200

    init basic corrections, loaders and filter

--- src/Tartaros.jl
+++ src/Tartaros.jl
@@ -1,5 +1,19 @@
 module Tartaros
 
-# Write your package code here.
+using DocStringExtensions: TYPEDSIGNATURES
+using Interpolations
+using NCDatasets: NCDatasets
+
+include("filters.jl")
+include("corrections.jl")
+include("loaders.jl")
+
+export AbstractSmoothingKernel, ConstantSmoothingKernel, GaussianSmoothingKernel
+export smooth
+
+export AbstractGHFCorrection, NoGHFCorrection, TopographicGHFCorrection
+export correct, correct!
+
+export GHFDataset, load_ghf, interpolate_ghf
 
 end
